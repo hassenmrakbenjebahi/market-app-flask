395884e355378345ca8c5680540b095b67090a2f
hassenmrakbenjebahi <123837615+hassenmrakbenjebahi@users.noreply.github.com>
2024-02-09 14:34:30 +0100

    template Inheritance

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 @app.route('/home')
-def home():
+def home_page():
     return render_template('home.html')
 @app.route('/market')
-def market_pages():
+def market_page():
     items = [
         {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
         {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
